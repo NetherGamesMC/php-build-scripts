a80a844b080129bdcb0a05eef0f24c2daf043500
larryTheCoder <larryPeter132@gmail.com>
2023-07-23 04:14:31 +0800

    Support for VanillaGenerator PM5

--- windows-compile-vs.bat
+++ windows-compile-vs.bat
@@ -43,7 +43,6 @@ set PHP_ENCODING_VER=0.2.3
 set PHP_VANILLAGENERATOR_VER=56fc48ea1367e1d08b228dfa580b513fbec8ca31
 set PHP_LIBKAFKA_VER=6.0.3
 set PHP_ZSTD_VER=0.12.3
-set PHP_GRPC_VER=59399d1958e8a6268e99a382f6ac3b027682f3da
 
 set script_path=%~dp0
 set log_file=%script_path%compile.log
@@ -172,9 +171,8 @@ cmake -P cmake_install.cmake >> "%log_file%" 2>&1 || exit 1
 cd /D "%DEPS_DIR%"
 
 call :pm-echo "Moving php-gRPC extension source..."
-call :get-zip "https://github.com/larryTheCoder/grpc/archive/%PHP_GRPC_VER%.zip" || exit 1
 
-move grpc-%PHP_GRPC_VER%\src\php\ext\grpc ..\php-src\ext\grpc >> "%log_file%" 2>&1 || exit 1
+move grpc\src\php\ext\grpc ..\php-src\ext\grpc >> "%log_file%" 2>&1 || exit 1
 move grpc\third_party\protobuf\php\ext\google\protobuf ..\php-src\ext\protobuf >> "%log_file%" 2>&1 || exit 1
 move grpc\third_party\protobuf\third_party ..\php-src\ext\protobuf\third_party >> "%log_file%" 2>&1 || exit 1
 cd ..\php-src\ext\protobuf
@@ -292,7 +290,7 @@ echo|(set /p="CHECK_LIB("absl_time_zone.lib", "grpc", PHP_GRPC);" & echo.) >> co
 echo|(set /p="CHECK_LIB("absl_bad_any_cast_impl.lib", "grpc", PHP_GRPC);" & echo.) >> config.w32
 echo|(set /p="CHECK_LIB("absl_bad_optional_access.lib", "grpc", PHP_GRPC);" & echo.) >> config.w32
 echo|(set /p="CHECK_LIB("absl_bad_variant_access.lib", "grpc", PHP_GRPC);" & echo.) >> config.w32
-echo|(set /p="EXTENSION("grpc", "batch.c byte_buffer.c call.c call_credentials.c channel.c channel_credentials.c completion_queue.c timeval.c server.c server_credentials.c php_grpc.c", PHP_GRPC_SHARED, "");" & echo.) >> config.w32
+echo|(set /p="EXTENSION("grpc", "byte_buffer.c call.c call_credentials.c channel.c channel_credentials.c completion_queue.c timeval.c server.c server_credentials.c php_grpc.c", PHP_GRPC_SHARED, "");" & echo.) >> config.w32
 echo|(set /p="AC_DEFINE('HAVE_GRPC', 1, '');" & echo.) >> config.w32
 echo|(set /p="} else {" & echo.) >> config.w32
 echo|(set /p="WARNING("php-grpc not enabled; libraries and headers not found");" & echo.) >> config.w32
